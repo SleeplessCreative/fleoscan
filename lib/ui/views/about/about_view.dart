5718bd47523bc6f17691714c8c03bc7aefe17a29
Muhammad Erald Setyaki Siregar <erald.muhammad@gmail.com>
2020-08-08 04:03:08 +0700

    feat : about

--- lib/ui/views/about/about_view.dart
+++ lib/ui/views/about/about_view.dart
@@ -1,3 +1,4 @@
+import 'package:fleoscan/items/fleocolor.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
@@ -10,8 +11,58 @@ class AboutView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<AboutViewModel>.reactive(
         builder: (context, model, child) => Scaffold(
-              body: Center(
-                child: Text(model.title),
+              body: Container(
+                width: double.infinity,
+                decoration: new BoxDecoration(
+                  color: FleoColor.c200(),
+                  boxShadow: [defaultShadow],
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 6,
+                      child: Container(
+                        padding: EdgeInsets.all(10.0),
+                        margin: EdgeInsets.all(10.0),
+                        child: model.logoFleoscan,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                          decoration: BoxDecoration(
+                            color: FleoColor.c600(),
+                            boxShadow: [defaultShadow],
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30),
+                            ),
+                          ),
+                          child: Center(
+                            child: Container(
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.all(10.0),
+                              child: RichText(
+                                textAlign: TextAlign.center,
+                                text: TextSpan(
+                                  style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 16.0,
+                                    color: FleoColor.c200(),
+                                  ),
+                                  children: <TextSpan>[
+                                    TextSpan(text: model.aboutText),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )),
+                    ),
+                  ],
+                ),
               ),
             ),
         viewModelBuilder: () => AboutViewModel());
--- lib/ui/views/about/about_viewmodel.dart
+++ lib/ui/views/about/about_viewmodel.dart
@@ -1,6 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:stacked/stacked.dart';
 
 class AboutViewModel extends BaseViewModel {
   String _title = 'This is title';
   String get title => _title;
+
+  Image _logoFleoscan = Image.asset(
+    'assets/logo_fleo.png',
+    width: 169,
+  );
+  Image get logoFleoscan => _logoFleoscan;
+
+  String _aboutText = '''
+Version 1.0
+
+Copyright (c) 
+2020 - Sleeplesscreative
+                            ''';
+  String get aboutText => _aboutText;
 }
