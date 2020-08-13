5690c8a9715f2dac8907bf825adf18a96595dde6
Muhammad Erald Setyaki Siregar <erald.muhammad@gmail.com>
2020-08-13 13:14:39 +0700

    style : appbar title, application label

--- lib/ui/widgets/appbar/appbar_viewmodel.dart
+++ lib/ui/widgets/appbar/appbar_viewmodel.dart
@@ -2,8 +2,6 @@ import 'package:stacked/stacked.dart';
 import 'package:flutter_svg/svg.dart';
 
 class AppbarViewModel extends BaseViewModel {
-  String get appbarTitle => 'FLEOSCAN';
-
   SvgPicture _burgerSvgPicture = SvgPicture.asset(
     'assets/burger.svg',
     width: 17,
